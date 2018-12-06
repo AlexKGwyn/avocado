8480089b21f9e5ccab583728a5b4b2da52af4efc
Alex Gwyn <agwyn@twitter.com>
2018-12-05 19:16:57 -0800

    Update together

--- src/plugins/convertAnimationSetsToTogether.ts
+++ src/plugins/convertAnimationSetsToTogether.ts
@@ -7,31 +7,31 @@ import { Plugin } from './_types';
  * https://medium.com/androiddevelopers/re-animation-7869722af206#7bd8
  */
 function fn(item: JsApi) {
-  if (
-    item.isElem('set') &&
-    item.hasAttr('android:ordering') &&
-    item.attr('android:ordering').value === 'sequentially'
-  ) {
-    let currentTime = 0;
-    for (const child of item.content) {
-      const startOffset = child.attr('android:startOffset');
-      const duration = child.attr('android:duration');
-      if (startOffset) {
-        currentTime += parseInt(startOffset.value, 10);
-        startOffset.value = currentTime.toString();
-      } else if (currentTime > 0) {
-        child.addAttr({
-          name: 'android:startOffset',
-          prefix: 'android',
-          local: 'startOffset',
-          value: currentTime.toString(),
-        });
-      }
-      if (duration) {
-        currentTime += parseInt(duration.value, 10);
+  if (item.isElem('set') && item.hasAttr('android:ordering')) {
+    if (item.attr('android:ordering').value === 'sequentially') {
+      let currentTime = 0;
+
+      // TODO needs to be recursive for nested sets
+      for (const child of item.content) {
+        const startOffset = child.attr('android:startOffset');
+        const duration = child.attr('android:duration');
+        if (startOffset) {
+          currentTime += parseInt(startOffset.value, 10);
+          startOffset.value = currentTime.toString();
+        } else if (currentTime > 0) {
+          child.addAttr({
+            name: 'android:startOffset',
+            prefix: 'android',
+            local: 'startOffset',
+            value: currentTime.toString(),
+          });
+        }
+        if (duration) {
+          currentTime += parseInt(duration.value, 10);
+        }
       }
     }
-    item.attr('android:ordering').value = 'together';
+    item.removeAttr('android:ordering');
   }
 
   return item;
